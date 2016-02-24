0fcd0e46d213c1cec79610019bd3041013ca90f6
blacktop <blacktop@users.noreply.github.com>
2016-02-23 23:27:54 -0700

    working on comodo

--- comodo/README.md
+++ comodo/README.md
@@ -1,38 +1,102 @@
-Comodo Dockerfile
-=============
+# malice-comodo
 
-This repository contains a **Dockerfile** of [Comodo](https://www.comodo.com/home/internet-security/antivirus-for-linux.php) for [Docker](https://www.docker.io/)'s [trusted build](https://index.docker.io/u/blacktop/comodo/) published to the public [Docker Registry](https://index.docker.io/).
+[![License](http://img.shields.io/:license-mit-blue.svg)](http://doge.mit-license.org)
+[![Docker Stars](https://img.shields.io/docker/stars/malice/comodo.svg)][hub]
+[![Docker Pulls](https://img.shields.io/docker/pulls/malice/comodo.svg)][hub]
+[![Image Size](https://img.shields.io/imagelayers/image-size/malice/comodo/latest.svg)](https://imagelayers.io/?images=malice/comodo:latest)
+[![Image Layers](https://img.shields.io/imagelayers/layers/malice/comodo/latest.svg)](https://imagelayers.io/?images=malice/comodo:latest)
+
+This repository contains a **Dockerfile** of [Comodo](https://www.comodo.com/home/internet-security/antivirus-for-linux.php) for [Docker](https://www.docker.io/)'s [trusted build][hub] published to the public [DockerHub](https://hub.docker.com).
 
 ### Dependencies
 
-* [ubuntu:latest](https://index.docker.io/_/ubuntu/)
+* [debian:jessie (*125 MB*)](https://hub.docker.com/_/debian/)
 
 ### Installation
 
 1. Install [Docker](https://www.docker.io/).
-2. Download [trusted build](https://index.docker.io/u/blacktop/comodo/) from public [Docker Registry](https://index.docker.io/): `docker pull blacktop/comodo`
+2. Download [trusted build][hub] from public [DockerHub](https://hub.docker.com): `docker pull malice/comodo`
 
-#### Alternatively, build an image from Dockerfile
-`docker build -t blacktop/comodo github.com/blacktop/docker-comodo`
 ### Usage
-```
-$ docker run -i -t blacktop/comodo -vs /malware/EICAR
-```
+
+    docker run -it --rm malice/comodo EICAR
+
 #### Or link your own malware folder:
 ```bash
-$ docker run -i -t -v /path/to/malware/:/malware:ro blacktop/comodo
+$ docker run -it --rm -v /path/to/file/:/malware:ro malice/comodo
+
+Usage: comodo [OPTIONS] COMMAND [arg...]
+
+Malice Comodo AntiVirus Plugin
+
+Version: v0.1.0, BuildTime: 20160214
+
+Author:
+  blacktop - <https://github.com/blacktop>
+
+Options:
+  --table, -t	output as Markdown table
+  --post, -p	POST results to Malice webhook [$MALICE_ENDPOINT]
+  --proxy, -x	proxy settings for Malice webhook endpoint [$MALICE_PROXY]
+  --help, -h	show help
+  --version, -v	print the version
+
+Commands:
+  update	Update virus definitions
+  help		Shows a list of commands or help for one command
+
+Run 'comodo COMMAND --help' for more information on a command.
 ```
-#### Output:
+
+This will output to stdout and POST to malice results API webhook endpoint.
+
+### Sample Output JSON:
+```json
+{
+  "comodo": {
+    "infected": true,
+    "result": "EICAR_Test_File (exact)",
+    "engine": "4.6.5.141",
+    "updated": "20160213"
+  }
+}
 ```
------== Scan Start ==-----
-/malware/EICAR ---> Found Virus, Malware Name is ApplicUnwnt
------== Scan End ==-----
-Number of Scanned Files: 1
-Number of Found Viruses: 1
+### Sample Output STDOUT (Markdown Table):
+---
+#### Comodo
+| Infected | Result                  | Engine    | Updated    |
+| -------- | ----------------------- | --------- | ---------- |
+| true     | EICAR_Test_File (exact) | 4.6.5.141 | 20160213   |
+---
+### To Run on OSX
+ - Install [Homebrew](http://brew.sh)
+
+```bash
+$ brew install caskroom/cask/brew-cask
+$ brew cask install virtualbox
+$ brew install docker
+$ brew install docker-machine
+$ docker-machine create --driver virtualbox malice
+$ eval $(docker-machine env malice)
 ```
-### Todo
-- [x] Install/Run Comodo
-- [ ] Start Daemon and watch folder with supervisord
-- [ ] Have container take a URL as input and download/scan file
-- [ ] Output Scan Results as formated JSON
-- [ ] Attach a Volume that will hold malware for a host's tmp folder
+
+### Documentation
+To update the AV run the following:
+```bash
+$ docker run --name=comodo malice/comodo update
+```
+Then to used the updated Comodo container:
+```bash
+$ docker restart comodo > /dev/null && docker exec comodo scan --table EICAR
+```
+
+### Issues
+
+Find a bug? Want more features? Find something missing in the documentation? Let me know! Please don't hesitate to [file an issue](https://github.com/maliceio/malice-av/issues/new) and I'll get right on it.
+
+### Credits
+
+### License
+MIT Copyright (c) 2016 **blacktop**
+
+[hub]: https://hub.docker.com/r/malice/comodo/
---
+++ comodo/scan.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/crackcomm/go-clitable"
+	"github.com/parnurzeal/gorequest"
+)
+
+// Version stores the plugin's version
+var Version string
+
+// BuildTime stores the plugin's build time
+var BuildTime string
+
+// Comodo json object
+type Comodo struct {
+	Results ResultsData `json:"comodo"`
+}
+
+// ResultsData json object
+type ResultsData struct {
+	Infected bool   `json:"infected"`
+	Result   string `json:"result"`
+	Engine   string `json:"engine"`
+	Updated  string `json:"updated"`
+}
+
+func getopt(name, dfault string) string {
+	value := os.Getenv(name)
+	if value == "" {
+		value = dfault
+	}
+	return value
+}
+
+func assert(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// RunCommand runs cmd on file
+func RunCommand(cmd string, args ...string) string {
+
+	cmdOut, err := exec.Command(cmd, args...).Output()
+	if len(cmdOut) == 0 {
+		assert(err)
+	}
+
+	return string(cmdOut)
+}
+
+// ParseComodoOutput convert comodo output into ResultsData struct
+func ParseComodoOutput(comodoout string) ResultsData {
+
+	comodo := ResultsData{Infected: false}
+	colonSeparated := []string{}
+
+	lines := strings.Split(comodoout, "\n")
+	// Extract Virus string and extract colon separated lines into an slice
+	for _, line := range lines {
+		if len(line) != 0 {
+			if strings.Contains(line, ":") {
+				colonSeparated = append(colonSeparated, line)
+			}
+			if strings.Contains(line, "[Found virus]") {
+				result := extractVirusName(line)
+				if len(result) != 0 {
+					comodo.Result = result
+					comodo.Infected = true
+				} else {
+					fmt.Println("[ERROR] Virus name extracted was empty: ", result)
+					os.Exit(2)
+				}
+			}
+		}
+	}
+	// fmt.Println(lines)
+
+	// Extract Comodo Details from scan output
+	if len(colonSeparated) != 0 {
+		for _, line := range colonSeparated {
+			if len(line) != 0 {
+				keyvalue := strings.Split(line, ":")
+				if len(keyvalue) != 0 {
+					switch {
+					case strings.Contains(keyvalue[0], "Virus signatures"):
+						comodo.Updated = parseUpdatedDate(strings.TrimSpace(keyvalue[1]))
+					case strings.Contains(line, "Engine version"):
+						comodo.Engine = strings.TrimSpace(keyvalue[1])
+					}
+				}
+			}
+		}
+	} else {
+		fmt.Println("[ERROR] colonSeparated was empty: ", colonSeparated)
+		os.Exit(2)
+	}
+
+	return comodo
+}
+
+// extractVirusName extracts Virus name from scan results string
+func extractVirusName(line string) string {
+	r := regexp.MustCompile(`<(.+)>`)
+	res := r.FindStringSubmatch(line)
+	if len(res) != 2 {
+		return ""
+	}
+	return res[1]
+}
+
+func printStatus(resp gorequest.Response, body string, errs []error) {
+	fmt.Println(resp.Status)
+}
+
+func parseUpdatedDate(date string) string {
+	layout := "200601021504"
+	t, _ := time.Parse(layout, date)
+	return fmt.Sprintf("%d%02d%02d", t.Year(), t.Month(), t.Day())
+}
+
+func printMarkDownTable(comodo Comodo) {
+
+	fmt.Println("#### Comodo")
+	table := clitable.New([]string{"Infected", "Result", "Engine", "Updated"})
+	table.AddRow(map[string]interface{}{
+		"Infected": comodo.Results.Infected,
+		"Result":   comodo.Results.Result,
+		"Engine":   comodo.Results.Engine,
+		"Updated":  comodo.Results.Updated,
+	})
+	table.Markdown = true
+	table.Print()
+}
+
+func updateAV() {
+	fmt.Println("Updating Comodo...")
+	// TODO: I need to download the update files
+	// http://download.comodo.com/av/updates58/sigs/bases/bases.cav /opt/COMODO/scanners/bases.cav
+}
+
+var appHelpTemplate = `Usage: {{.Name}} {{if .Flags}}[OPTIONS] {{end}}COMMAND [arg...]
+
+{{.Usage}}
+
+Version: {{.Version}}{{if or .Author .Email}}
+
+Author:{{if .Author}}
+  {{.Author}}{{if .Email}} - <{{.Email}}>{{end}}{{else}}
+  {{.Email}}{{end}}{{end}}
+{{if .Flags}}
+Options:
+  {{range .Flags}}{{.}}
+  {{end}}{{end}}
+Commands:
+  {{range .Commands}}{{.Name}}{{with .ShortName}}, {{.}}{{end}}{{ "\t" }}{{.Usage}}
+  {{end}}
+Run '{{.Name}} COMMAND --help' for more information on a command.
+`
+
+func main() {
+	cli.AppHelpTemplate = appHelpTemplate
+	app := cli.NewApp()
+	app.Name = "comodo"
+	app.Author = "blacktop"
+	app.Email = "https://github.com/blacktop"
+	app.Version = Version + ", BuildTime: " + BuildTime
+	app.Compiled, _ = time.Parse("20060102", BuildTime)
+	app.Usage = "Malice Comodo AntiVirus Plugin"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "table, t",
+			Usage: "output as Markdown table",
+		},
+		cli.BoolFlag{
+			Name:   "post, p",
+			Usage:  "POST results to Malice webhook",
+			EnvVar: "MALICE_ENDPOINT",
+		},
+		cli.BoolFlag{
+			Name:   "proxy, x",
+			Usage:  "proxy settings for Malice webhook endpoint",
+			EnvVar: "MALICE_PROXY",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:    "update",
+			Aliases: []string{"u"},
+			Usage:   "Update virus definitions",
+			Action: func(c *cli.Context) {
+				updateAV()
+			},
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		path := c.Args().First()
+
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			assert(err)
+		}
+
+		comodo := Comodo{
+			Results: ParseComodoOutput(RunCommand("/opt/COMODO/cmdscan", path)),
+		}
+
+		if c.Bool("table") {
+			printMarkDownTable(comodo)
+		} else {
+			comodoJSON, err := json.Marshal(comodo)
+			assert(err)
+			if c.Bool("post") {
+				request := gorequest.New()
+				if c.Bool("proxy") {
+					request = gorequest.New().Proxy(os.Getenv("MALICE_PROXY"))
+				}
+				request.Post(os.Getenv("MALICE_ENDPOINT")).
+					Set("Task", path).
+					Send(comodoJSON).
+					End(printStatus)
+			}
+			fmt.Println(string(comodoJSON))
+		}
+	}
+
+	err := app.Run(os.Args)
+	assert(err)
+}
